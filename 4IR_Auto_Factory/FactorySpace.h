1cdd71238b758eed26a416bd8467a62ac81029b3
kennethmokhethi <kennethmahlatsemokhethi@gmail.com>
2019-07-28 15:09:22 +0200

    Added the enumerations of the game together with functions prototypes and structures

--- 4IR_Auto_Factory/FactorySpace.h
+++ 4IR_Auto_Factory/FactorySpace.h
@@ -1,6 +1,68 @@
 #ifndef FACTORYSPACE_H_INCLUDED
 #define FACTORYSPACE_H_INCLUDED
 #include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+namespace factorySpace ///Creation of a namespace
+{
+    ///Type definations of the pointer
+    typedef int* t_1DArray;
+    typedef int** t_2DArray;
+
+    ///Enumerations for return codes
+    enum enumReturn_Codes
+    {
+        SUCCESS,
+        INVALID_INPUT,
+        INVALID_ARGS,
+        EXIT
+    };
+
+    ///Enumeration of the features
+    enum enumFeatures
+    {
+        PLAYER,
+        SPACE,
+        FACRORY,
+        PARTS
+    };
+
+    ///Character of the features of the game
+    const char CH_FEATURES[]={'P','~','#','@'};
+
+    ///Structure of cordinates
+    struct struLocation
+    {
+        int row=-1;
+        int col=-1;
+    };
+
+    ///Structure of the game
+    struct struGameWorld
+    {
+        struLocation Player;
+        int row=-1;
+        int col=-1;
+        t_2DArray arrWorld;
+    };
+
+  ///function of the game
+  void validateArgs(int argc,char** argv);
+  void convertToInt(string strNumber);
+  int genRandom(int intMin,int intMax);
+  struGameWorld makeWorld(int intRow,int intCol,int intParts);
+  void displayWorld(const struGameWorld& recworld);
+  bool isInWorld(struGameWorld& recworld);
+  void movePlayer(struGameWorld& recworld);
+  void deAllocateMemory(struGameWorld& recworld);
+
+
+
+
+
+}
 
 
 #endif // FACTORYSPACE_H_INCLUDED
