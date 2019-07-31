730900d12ea6e68a713935b4596c246867669b2b
kennethmokhethi <kennethmahlatsemokhethi@gmail.com>
2019-07-31 13:14:18 +0200

    implemented the the functions that places the parts in the factory

--- 4IR_Auto_Factory/FactorySpace.h
+++ 4IR_Auto_Factory/FactorySpace.h
@@ -8,14 +8,14 @@
 #include <cassert>
 
 using namespace std;
-namespace factorySpace ///Creation of a namespace
+namespace factorySpace ///Creation of a namespace.
 {
-    ///Type definations of the pointerS
+    ///Type definations of the pointers.
     typedef int* t_1DArray;
     typedef int** t_2DArray;
 
 
-    ///Enumerations for return codes
+    ///Enumerations for return codes.
     enum enumReturn_Codes
     {
         SUCCESS,
@@ -27,7 +27,7 @@ namespace factorySpace ///Creation of a namespace
         EXIT
     };
 
-    ///Enumeration of the features
+    ///Enumeration of the features.
     enum enumFeatures
     {
         PLAYER,
@@ -36,20 +36,21 @@ namespace factorySpace ///Creation of a namespace
         PARTS
     };
 
-    ///Character of the features of the game
+    ///Character of the features of the game.
     const char CH_FEATURES[]={'P','~','#','@'};
 
-    ///Structure of cordinates
+    ///Structure of cordinates.
     struct struLocation
     {
         int row=-1;
         int col=-1;
     };
 
-    ///Structure of the game
+    ///Structure of the game.
     struct struGameWorld
     {
         struLocation Player;
+        struLocation Factory;
         int row=-1;
         int col=-1;
         int parts=-1;
@@ -58,7 +59,7 @@ namespace factorySpace ///Creation of a namespace
         t_2DArray arrWorld;
     };
 
-  ///function of the game
+  ///functions of the game.
   void validateArgs(int argc,char** argv);
   int convertToInt(string strNumber);
   int genRandom(int intMin,int intMax);
@@ -68,10 +69,6 @@ namespace factorySpace ///Creation of a namespace
   void movePlayer(struGameWorld& recworld,char chInput);
   void outcome(struGameWorld& recworld);
   void deAllocateMemory(struGameWorld& recworld);
-
-
-
-
 
 }
 
